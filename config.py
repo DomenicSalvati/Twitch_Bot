a49dea468c355cbdc8b86ecc871fcabdcc70faf9
DomenicSalvati <43186177+DomenicSalvati@users.noreply.github.com>
2020-02-25 21:25:23 -0800

    Twitch Bot

---
+++ config.py
@@ -0,0 +1,18 @@
+HOST = "irc.chat.twitch.tv"              # This is Twitchs IRC server
+PORT = 6667                         # Twitchs IRC server listens on port 6767
+NICK = "187236489172"           # Twitch username your using for your bot
+PASS = "oauth:3cl3ybwwq8x28fazup7onx4w2q4bqs" # your Twitch OAuth token
+CHAN = "#renongaming"                   # the channel you want the bot to join.
+RATE = (20/30) # messages per seccond
+BAN_PAT = [
+]
+
+TO_PAT = [
+]
+
+COM_PAT = {
+        r"brett" : r"is NOT gay",
+        r"dom" : r"created this bot",
+        r"chris" :  r"says the n word"
+}
+
